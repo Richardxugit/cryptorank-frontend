1e7f378d836420bb1dc1aa6de811624ac102f4b9
Richardxugit <cansolion@163.com>
2021-01-22 16:51:14 +1100

    basic layout and connect to the api

--- coinrank/src/App.js
+++ coinrank/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { coinList: [] };
+  }
+  async componentDidMount() {
+    const url = `localhost:8080/api/currency`;
+    const result = await axios.get(url)
+    const { data } = result
+    console.log(data)
+    this.SetState({ data });
+  }
+  render() {
+    return (
+      <div className="App">
+        app
+      </div>
+    );
+  }
 }
 
 export default App;
